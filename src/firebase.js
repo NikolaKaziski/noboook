2f47772e5c8bb6201dde850b404745c1df86722e
Nikola Kaziski <nikolakaziski@gmail.com>
2021-03-11 12:12:20 +0200

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANPY74bnZ1YQmICMrYZKiLrw9RgYFnu74",
+    authDomain: "noboook-4e0c8.firebaseapp.com",
+    projectId: "noboook-4e0c8",
+    storageBucket: "noboook-4e0c8.appspot.com",
+    messagingSenderId: "701271651460",
+    appId: "1:701271651460:web:a4375ad07931b09f9efed1"
+};
