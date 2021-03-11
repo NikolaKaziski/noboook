ec7f31aab468ef5fcf6ff1d34276093d88d6863c
Nikola Kaziski <nikolakaziski@gmail.com>
2021-03-11 13:50:49 +0200

    adding material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Something new</h1>
+
+      <Header/>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,17 @@ import "./Header.css";
 function Header() {
     return (
         <div className="header">
-            
+            <div className="header__left">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
+                    alt=""
+                />
+            </div>
+
+            <div className="header__middle"></div>
+
+            <div className="header__right"></div>
+
         </div>
     )
 }
