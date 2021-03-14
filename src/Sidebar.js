2921734091adf20076100f989028e8d5be02e483
Nikola Kaziski <nikolakaziski@gmail.com>
2021-03-14 10:14:48 +0200

    adding avatar and picture in sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Sidebar.css';
 import SidebarRow from "./SidebarRow"
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
 import PeopleIcon from '@material-ui/icons/People';
 import ChatIcon from '@material-ui/icons/Chat';
@@ -9,16 +10,20 @@ import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 import { ExpandMoreOutlined } from "@material-ui/icons";
 
 function Sidebar() {
-    return <div className="sidebar">
-       
-        <SidebarRow Icon={EmojiFlagsIcon} title='Pages' />
-        <SidebarRow Icon={PeopleIcon} title='Friends' />
-        <SidebarRow Icon={ChatIcon} title='Messenger' />
-        <SidebarRow Icon={StorefrontIcon} title='Marketplace' />
-        <SidebarRow Icon={VideoLibraryIcon} title='Videos' />
-        <SidebarRow Icon={ExpandMoreOutlined} title='Marketplace' />
+    return (
+        <div className="sidebar">
+            <SidebarRow 
+            src='https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/GettyImages-1092658864_thumb-732x549.jpg' title="boy"/>
+            <SidebarRow Icon={LocalHospitalIcon} title='Covid 19 Information Center' />
+            <SidebarRow Icon={EmojiFlagsIcon} title='Pages' />
+            <SidebarRow Icon={PeopleIcon} title='Friends' />
+            <SidebarRow Icon={ChatIcon} title='Messenger' />
+            <SidebarRow Icon={StorefrontIcon} title='Marketplace' />
+            <SidebarRow Icon={VideoLibraryIcon} title='Videos' />
+            <SidebarRow Icon={ExpandMoreOutlined} title='Marketplace' />
 
-    </div>
-};
+        </div>
+    );
+}
 
 export default Sidebar;
