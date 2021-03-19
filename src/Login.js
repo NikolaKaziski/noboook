a08cb67a3e87bfad3c64bddccbff6877f2fb6fc0
Nikola Kaziski <nikolakaziski@gmail.com>
2021-03-19 20:23:37 +0200

    update login

--- src/Login.js
+++ src/Login.js
@@ -2,8 +2,12 @@ import { Button } from '@material-ui/core';
 import React from 'react'
 import './Login.css';
 import { auth, provider } from "./firebase";
+import { actionTypes } from "./reducer";
+import { useStateValue } from './StateProvider';
 
 function Login() {
+    const [state,dispatch] = useStateValue();
+
     const singIn = () => {
         auth
             .signInWithPopup(provider)
