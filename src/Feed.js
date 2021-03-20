0967edb484d286d1ecabe81b6fb66964745cda5d
Nikola Kaziski <nikolakaziski@gmail.com>
2021-03-20 16:46:44 +0200

    fixing timestamp

--- src/Feed.js
+++ src/Feed.js
@@ -20,7 +20,7 @@ function Feed() {
 
             {posts.map((post) => (
                 <Post
-                    key={post.data.id}
+                    key={post.id}
                     profilePic={post.data.profilePic}
                     message={post.data.message}
                     timestamp={post.data.timestamp}
