6a3b0d8c667bfa7bcbbce72c928bed600fe85d64
Nikola Kaziski <nikolakaziski@gmail.com>
2021-03-20 16:27:50 +0200

    creating firestore

--- src/Feed.js
+++ src/Feed.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
 import Post from './Post';
+import db from "./firebase";
 
 function Feed() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        db.collection("posts").onSnapshot((snapshot) =>
+            setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+        );
+    }, []);
     return (
         <div className='feed'>
             <StoryReel />
             <MessageSender />
 
-            <Post 
-            profilePic="https://mymodernmet.com/wp/wp-content/uploads/2021/01/morphy-me-celebrity-face-mashups-15.jpg"
-            message="it works"
-            timestamp="21.00"
-            username="random person"
-            image="https://cdn-images.welcometothejungle.com/5DDbrp9_kdlw05Z0hzc7kYEpaaruHhUUWJqs-nW1o8k/rs:auto:980::/q:85/czM6Ly93dHRqLXByb2R1Y3Rpb24vdXBsb2Fkcy9jYXRlZ29yeS9jb3Zlci8yNjYwLzE1NDg4My9jb2xsZWN0aW9uX2NhdGVnb3J5X2JlaGluZF90aGVfY29kZS5qcGc"
+            <Post
+                profilePic="https://mymodernmet.com/wp/wp-content/uploads/2021/01/morphy-me-celebrity-face-mashups-15.jpg"
+                message="it works"
+                timestamp="21.00"
+                username="random person"
+                image="https://cdn-images.welcometothejungle.com/5DDbrp9_kdlw05Z0hzc7kYEpaaruHhUUWJqs-nW1o8k/rs:auto:980::/q:85/czM6Ly93dHRqLXByb2R1Y3Rpb24vdXBsb2Fkcy9jYXRlZ29yeS9jb3Zlci8yNjYwLzE1NDg4My9jb2xsZWN0aW9uX2NhdGVnb3J5X2JlaGluZF90aGVfY29kZS5qcGc"
             />
-            <Post 
-            profilePic="https://c0.anyrgb.com/images/861/570/people-portrait-woman-human-person-face-girl-female-fashion-reference.jpg"
-            message="it works"
-            timestamp="21.00"
-            username="random girl"
-            image="https://code.org/shared/images/social-media/codeorg2020_social.png"
+            <Post
+                profilePic="https://c0.anyrgb.com/images/861/570/people-portrait-woman-human-person-face-girl-female-fashion-reference.jpg"
+                message="it works"
+                timestamp="21.00"
+                username="random girl"
+                image="https://code.org/shared/images/social-media/codeorg2020_social.png"
             />
-            
+
         </div>
     )
 }
