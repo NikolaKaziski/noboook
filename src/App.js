6616c2a76c4842be6e64b7a377c6aad0f6430f57
Nikola Kaziski <nikolakaziski@gmail.com>
2021-03-31 22:27:19 +0300

    making contactus.js and some routing

--- src/App.js
+++ src/App.js
@@ -5,29 +5,38 @@ import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Widgets from './Widgets';
 import Login from './Login';
+import ContactUs from './ContactUs';
 import { useStateValue } from './StateProvider';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
-  
+
   return (
-    <div className="app">
-      {!user ? (
-        <Login />
-      ) : (
-        <>
+    <Router>
+      <div className="app">
+        {!user ? (
+          <Login />
+        ) : (
+          <>
 
-          <Header />
+            <Header />
 
-          <div className="app__body">
-            <Sidebar />
-            <Feed />
-            <Widgets />
-          </div>
-        </>
-      )}
-    </div>
+            <Switch>
+              <Route path="/" exact>
+                <div className="app__body">
+                  <Sidebar />
+                  <Feed />
+                  <Widgets />
+                </div>
+              </Route>
+              <Route path="/contact" component={ContactUs} />
+            </Switch>
+          </>
+        )}
+      </div>
+    </Router >
   );
 }
 
---
+++ src/ContactUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactUs() {
+    return (
+        <div>
+            <h1>Hello, please contact us</h1>
+        </div>
+    )
+}
+
+export default ContactUs
