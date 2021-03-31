3f73cc6694991d1d014030127212cff8cf83f63e
Nikola Kaziski <nikolakaziski@gmail.com>
2021-03-31 23:50:11 +0300

    more work on contactUs, but need to be fix

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
 
 function ContactUs() {
     return (
-        <div>
-            <h1>Hello, please contact us</h1>
+        <div className="block contactBlock">
+            <div className="container-fluid">
+                <div className="titleHolder">
+                    <h2>Contact Us</h2>
+                    <p>If you have any question please contact us</p>
+                </div>
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                    
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your Username!' }]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your Password!' }]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+
+                        <a className="login-form-forgot" href="">
+                            Forgot password
+                        </a>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                    </Button>
+                        Or <a href="">register now!</a>
+                    </Form.Item>
+                </Form>
+            </div>
         </div>
     )
 }
 
-export default ContactUs
+export default ContactUs;
